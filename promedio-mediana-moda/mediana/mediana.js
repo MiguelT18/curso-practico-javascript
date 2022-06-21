c23b6daddcd017b70d78dab012a5ac11fd88e273
Miguel Teran <miguel.teranj02@gmail.com>
2022-06-20 21:30:12 -0400

    taller de mediana y moda terminados (convertir el codigo de moda en funciones)

---
+++ promedio-mediana-moda/mediana/mediana.js
@@ -0,0 +1,36 @@
+function calculateAverage(list) {
+  const sumList = list.reduce(function (accumulatedValue = 0, newElement) {
+    return accumulatedValue + newElement;
+  });
+
+  const averageList = sumList / list.length;
+  return averageList;
+}
+
+function calculateMedian(list) {
+  list.sort();
+
+  const halfList = parseInt(list.length / 2);
+
+  function esPar(number) {
+    if (number % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  let median;
+
+  if (esPar(list.length)) {
+    const element1 = list[halfList - 1];
+    const element2 = list[halfList];
+
+    const averageElement1n2 = calculateAverage([element1, element2]);
+
+    median = averageElement1n2;
+  } else {
+    median = list[halfList];
+  }
+  return median;
+}
--- promedio-mediana-moda/promedio/promedio.js
+++ promedio-mediana-moda/promedio/promedio.js
@@ -2,11 +2,15 @@
 
 // Para llamar a la función utilizar: calculateAverage([]);
 function calculateAverage(list) {
-  let sumList = 0;
+  // let sumList = 0;
 
-  for (let i = 0; i < list.length; i++) {
-    sumList = sumList + list[i];
-  }
+  // for (let i = 0; i < list.length; i++) {
+  //   sumList = sumList + list[i];
+  // }
+
+  const sumList = list.reduce(function (accumulatedValue = 0, newElement) {
+    return accumulatedValue + newElement;
+  });
 
   const averageList = sumList / list.length;
   return averageList;
